9e0c2d17a77c79751428db31bfdad305e499489d
Kazuaki Masuda <kame.kazu.0213.masumerry.0112@gmail.com>
2015-01-30 06:10:23 +0900

    たぶん最終

--- sfc_class/jokiso2_final/final.js
+++ sfc_class/jokiso2_final/final.js
@@ -4,7 +4,7 @@ var names = [];
 var username, myname; //相手、自分
 
 function response(req, res) {
-	var template = fs.readFileSync("final.ejs", "utf8");
+	var template = fs.readFileSync("final.ejs", "utf-8");
 	var html = ejs.render(template, {});
 	res.writeHead(200, {"Content-type": "text/html"});
 	res.write(html);
@@ -29,9 +29,16 @@ function read_talk(err, content){
 		for(var i = 0; i < talks.length; ++i){
 			talks[i] = qs.parse(talks[i]);
 		}
+		console.log(talks);
 		io.sockets.emit("talkdata",{value: talks});
 	}
 }
+
+fs.readFile("./talk/talker.txt", "utf-8", function(err, name){
+			//console.log(name);
+			names = name.split("\n");
+			console.log("array", names);
+		});
 
 io.sockets.on("connection", function(sock){
 
@@ -45,13 +52,27 @@ io.sockets.on("connection", function(sock){
 		}
 	});*/
 	sock.on("newname", function(name){
+		console.log("new_name");
+		var newwhich = true;
+		console.log("for start");
+		for(var i = 0; i < names.length; ++i){
+			console.log(names[i],name.value,names[i] == name.value);
+			if(names[i] == name.value) {
+				newwhich = false; 
+				console.log("same_here");
+				break;
+			}
+		}
+		if(newwhich){
 		fs.appendFile("./talk/talker.txt", "\n" + name.value, function(err){
 			if(err) throw err;
 		});
+		}
 		fs.readFile("./talk/talker.txt", "utf-8", function(err, name){
-			console.log(name);
+			console.log("newlist",name,"new_end");
 			io.sockets.emit("names",{value: name});
 		});
+		sock.emit("newname_ok", {value: myname});
 
 	});
 
@@ -69,10 +90,39 @@ io.sockets.on("connection", function(sock){
 		myname == myname_recv.value;
 		var talkpath = "./talk/talk.txt";
 		fs.readFile("./talk/talker.txt", "utf-8", function(err, name){
-			console.log(name);
+			//console.log(name);
 			io.sockets.emit("names",{value: name});
 		});
 	});
+
+	sock.on("talk_read", function(num){
+		var numarray = num.value;
+		fs.readFile(talkpath, "utf-8", function(err, content){
+				if(err){
+					console.log("Error," + err.code);
+				}else{
+					talks = String(content).split("\n");
+					for(var i = 0; i < talks.length; ++i){
+						talks[i] = qs.parse(talks[i]);
+					}
+					console.log("talks",talks);
+					for(var i = 0; i < numarray.length; ++i){
+						talks[numarray[i]].read = true;
+					}
+					console.log(talks);
+					var stringtalkarray = [];
+					for(var n = 0; n < talks.length; ++n){
+						stringtalkarray[n] = qs.stringify(talks[n]);
+					}
+					var stringtalk = stringtalkarray.join("\n");
+					console.log(stringtalk);
+					console.log(stringtalkarray);
+					fs.writeFile(talkpath, stringtalk, function(err){
+						console.log(err);
+					})
+				}
+		})
+	})
 
 	sock.on("talk_req",function(bang){
 		fs.readFile(talkpath, "utf-8", read_talk);
